d04830ff70cad0ee6b27c221fa6ff74a198e2ba7
samaboss2 <83290430+samaboss2@users.noreply.github.com>
2021-04-27 13:53:32 +0000

    Add files via upload

---
+++ samaboss.php
@@ -0,0 +1,121 @@
+<link rel="stylesheet" type="text/css" href="sam.css">
+<body>
+	<!-- dans chaque container on a trois colonne une seule ligne dont chaque colonne contient une classe
+		pour le css, une img avec une hauteur fixer a une valeur et une largeur auto dans le code html,
+		 le nom du produit, le prix, lachat du produit ainsi que la source du site de l'insertion-->
+	<div class="container">
+		<div class="a">
+			<figure><img src="ordinateur.jpg" height="400" width="auto">
+				<img src="sam6.png" height="140" width="auto" class="logo">
+
+			<figcaption class="nom">ordinateur</figcaption>
+
+			<figcaption class="prix">150.000F</figcaption>
+
+			<figcaption class="achat"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+
+			<figcaption class="source">Ou Appeller au 77-484-34-69</figcaption></figure>
+
+	</div>
+
+		<div class="b">
+			<figure><img src="ordinateur.jpg" height="400" width="auto">
+				<img src="sam6.png" height="140" width="auto" class="logo">
+
+			<figcaption class="nom">ordinateur</figcaption>
+
+			<figcaption class="prix">150.000F</figcaption>
+
+			<figcaption class="achat"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+
+			<figcaption class="source">Ou Appeller au 77-484-34-69</figcaption></figure></div>
+
+		<div class="c"><figure><img src="ordinateur.jpg" height="400" width="auto">
+				<img src="sam6.png" height="140" width="auto" class="logo">
+			<figcaption class="nom">scooter-electrique</figcaption>
+
+			<figcaption class="prix">450.000F</figcaption>
+
+			<figcaption class="achat"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+
+			<figcaption class="source">Ou Appeller au 77-484-34-69</figcaption></figure></div>
+
+	</div>
+
+	<div class="container2">
+		<div class="a"><figure><img src="ordinateur.jpg" height="350" width="auto">
+			<img src="sam6.png" height="140" width="auto" class="logo1">
+
+			<figcaption class="nom1">Ordinateur-portable</figcaption>
+
+			<figcaption class="prix1">150.000F</figcaption>
+									<!--le lien pour l'achat du produit-->
+			<figcaption class="achat1"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+											<!--le lien vers le site du-->
+			<figcaption class="source1">Ou Appeller au 77-484-34-69</figcaption></figure>
+
+		</div>
+
+		<div class="b">
+			<figure><img src="ordinateur.jpg" height="350" width="auto">
+				<img src="sam6.png" height="140" width="auto" class="logo1">
+
+			<figcaption class="nom1">ordinateur</figcaption>
+
+			<figcaption class="prix1">150.000F</figcaption>
+
+			<figcaption class="achat1"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+
+			<figcaption class="source1">Ou Appeller au 77-484-34-69</figcaption></figure></div>
+
+		<div class="c"><figure><img src="ordinateur.jpg" height="350" width="auto">
+				<img src="sam6.png" height="140" width="auto" class="logo1">
+			<figcaption class="nom1">scooter-electrique</figcaption>
+
+			<figcaption class="prix1">450.000F</figcaption>
+
+			<figcaption class="achat1"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+
+			<figcaption class="source1">Ou Appeller au 77-484-34-69</figcaption></figure></div>
+
+	</div>
+	
+	<div class="container3">
+		<div class="g">
+			<img src="telephone.jpg" height="200" width="auto">
+			<div class="nm">telephone</div>
+				<div class="pr">65.000 F</div>
+				<figcaption class="achat3"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+		</div>
+
+		<div class="h"><img src="hyundai-ix-35.jpg" height="200" width="auto">
+			<div class="nm">hyundai-ix-35</div>
+				<div class="pr">10.500.000 F</div>
+				<figcaption class="achat3"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+		</div>		
+
+		<div class="i"><img src="Samsung-Galaxy-S10.jpg" height="200" width="auto">
+			<div class="nm">Samsung-Galaxy-S10.jpg</div>
+				<div class="pr">85.000 F</div>
+				<figcaption class="achat3"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+		</div>		
+
+		<div class="j"><img src="scooter-electrique.jpg" height="200" width="auto">
+			<div class="nm">scooter-electrique</div>
+				<div class="pr">600.000 F</div>
+				<figcaption class="achat3"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+		</div>		
+
+		<div class="k"><img src="scooters-yamahaa-electriques.jpg" height="200" width="auto">
+			<div class="nm">scooters-yamahaa-electriques</div>
+				<div class="pr">650.000 F</div>
+				<figcaption class="achat3"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+		</div>		
+
+	<div class="l"><img src="tv.jpg" height="200" width="auto">
+			<div class="nm">TV</div>
+				<div class="pr">65.000 F</div>
+				<figcaption class="achat3"><a href="https://wa.me/221703538973">Envoyer un Message WhatsApp</a></figcaption>
+	</div>
+	<br><br>
+</body>
